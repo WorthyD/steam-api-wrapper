130ecafb8b3c909bb3b1a1511e9f7496cd7881fc
Daniel Worthy <danieljworthy@gmail.com>
2016-01-04 05:49:54 -0600

    stuff

---
+++ SteamApiWrapper/SteamApiWrapper/ReadMe.md
@@ -0,0 +1,12 @@
+﻿~~3.2 GetNewsForApp (v0002)~~
+~~3.3 GetGlobalAchievementPercentagesForApp (v0002)~~
+~~3.4 GetGlobalStatsForGame (v0001)~~
+~~3.5 GetPlayerSummaries (v0002)~~
+~~3.6 GetFriendList (v0001)~~
+~~3.7 GetPlayerAchievements (v0001)~~
+3.8 GetUserStatsForGame (v0002)
+3.9 GetOwnedGames (v0001)
+3.10 GetRecentlyPlayedGames (v0001)
+3.11 IsPlayingSharedGame (v0001)
+3.12 GetSchemaForGame (v2)
+3.13 GetPlayerBans (v1)
---
+++ SteamApiWrapper/SteamApiWrapper/SteamUserStats/GetUserStatsForGameResponse.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SteamApiWrapper.SteamUserStats
+{
+    public class GetUserStatsForGameResponse :Response
+    {
+        public Models.StatsForGame.Playerstats PlayerStats { get; set; }
+
+        public override void Convert()
+        {
+            var app = JsonConvert.DeserializeObject<Models.StatsForGame>(base.RawResponse);
+            if (app != null && app.playerstats != null)
+            {
+                this.PlayerStats = app.playerstats;
+            }
+            else
+            {
+                this.Status = ResponseStatus.ResponseStatusCode.NotFound;
+                this.StatusMessage = "No Stats Found";
+            }
+        }
+    }
+}
